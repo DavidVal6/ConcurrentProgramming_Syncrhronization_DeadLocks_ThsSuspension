be18b63143ecc4e919f3e2cc799ef66d753c4cd2
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2023-09-08 14:31:03 -0500

    Parte de Santiago

--- inmortals/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ inmortals/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -9,26 +9,38 @@ import java.util.Queue;
 
 /**
  *
- * @author hcadavid
+ * @author davidValencia
  */
-public class Consumer extends Thread{
-    
+public class Consumer extends Thread {
+
     private Queue<Integer> queue;
-    
-    
-    public Consumer(Queue<Integer> queue){
-        this.queue=queue;        
+    private final Object monitor;
+
+    public Consumer(Queue<Integer> queue, Object monitor) {
+        this.queue = queue;
+        this.monitor = monitor;
     }
-    
+
     @Override
     public void run() {
         while (true) {
+            synchronized (monitor) {
+                // Verificar si la cola está vacía
+                while (queue.isEmpty()) {
+                    try {
+                        monitor.wait(); // Esperar a que haya elementos en la cola
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
 
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+                int elem = queue.poll();
+                System.out.println("Consumer consumes " + elem);
+
+                monitor.notify(); // Notificar al productor que hay espacio en la cola
             }
-            
         }
     }
 }
+
+
